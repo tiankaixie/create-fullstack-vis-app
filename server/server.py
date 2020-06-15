191279c79dfd28161c5d0410bdcf3a505e4047c6
Tiankai Xie <tiankaixie@gmail.com>
2020-06-15 16:50:19 -0700

    server updated

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, escape, request, json, jsonify
+import networkx as nx
 
 app = Flask(__name__)
 
@@ -26,12 +27,21 @@ def test():
 
 
 @app.route('/getData/', methods=['POST'])
-def test():
-
+def get_data():
     request_raw = request.get_json()
-    print("get" + request_raw["name"] + "api is triggered")
+    print("get " + request_raw["name"] + " api is triggered")
+    G = nx.karate_club_graph()
+    nodes = []
+    for node in G.nodes():
+        nodes.append({"node_id": node})
+    edges = []
+    for edge in G.edges():
+        edges.append({"source": edge[0], "source_id": edge[0], "target": edge[1], "target_id": edge[1]})
+        edges.append({"source": edge[1], "source_id": edge[1], "target": edge[0], "target_id": edge[0]})
     res = {
-        "name": request_raw["name"]
+        "name": request_raw["name"],
+        "nodes": nodes,
+        "edges": edges
     }
     return jsonify(res)
 
